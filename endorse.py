d2463643f2618d6d280f0707a0607af511f87a25
Bob <rgmhtt@users.noreply.github.com>
2025-03-18 03:45:09 -0400

    Unified signing script

--- endorse.py
+++ endorse.py
@@ -2,62 +2,126 @@
 
 # HTT Consulting, LLC
 # Robert Moskowitz
-# 2023-05-7
+# 2025-03-16
 
-# developed with Fedora 37 using
+# developed with Fedora 41 using
 # dnf install python3-pycryptodomex
 # https://pycryptodome.readthedocs.io/en/v3.15.0/src/introduction.html
-# dnf install python3-pynacl
+# Two manditory files and one optional file needed.
 
-# one manditory file and one optional file needed.
-
-# Manditory is a pem private key file
+# First manditory file is a pem private key file of the CA/signer
 #		File name is <something>prv.pem
-#		<something> is the variable pkeyname
+#			Passwording this file is not yet implemented
 
-# Optional file is a file of commands to set the various variables
-#	The file name is --commandfile=<something> on the command line
-#		defautl is det.dat
+# Second manditory file is a file of commands to set the various server variables
+#	The file name is --serverdat=<something> on the command line
+#		default is server.dat
 #
 # sample content is:
 #
-#vnb="04/01/2023"
-#vna="04/01/2024"
-#DETofC=0x20010030000000050eda8a644093aadd
-#HIofC=0xf0beded7c542fcefd620f5f3f3b5f95627f50a308e1dacfe507adc62c322101c
-#DETofP=0x20010030000000050eda8a644093aadd
-#pkeyname=root
+#raa = 16376
+#hda = 16376 - HDA of server
+#serialnumberbits = 7
+#cakey="raa16376"
+#LOA_str = "1.3.27.16.1.1.0.1"
+#selfsign = False
+#certsign = False - set to true to include in CA cert
 
-# all of these variables can be overridden via the command line
+# Optional file is a file of commands to set the various variables
+#	The file name is --commandfile=<something> on the command line
+#		default is ua.dat
 #
-# e.g.
-#python endorse.py --commandfile=root.dat --vnb="06/01/2023" --pkeyname=root
+# sample content is:
 #
-# for self-endorsements (--self=y on command line):
-#	use HIofC for the HI of the signer, DET0fP and private pem key of signer as well.
+#hda = 16376 - HDA of client
+#vnb="04/01/2024"
+#vna="04/01/2025"
+#clientcsr="hda16376-16376A"
+#clientpem="hda16376-16376A"
+#caname = "RAA-A-16376-16376"
+#entitycert=True
+# caname when entitycert=False
 
-__version__ = '2023.05.01'
+__version__ = '2025-03-16'
 
 import sys, getopt
+import ipaddress
 import time
 import datetime
+import random
 from binascii import *
+import binascii
 import base64
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
+from cryptography import x509
+from cryptography.x509.oid import NameOID, ObjectIdentifier
+from cryptography.x509 import CertificatePolicies, PolicyInformation
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from Cryptodome.Hash import cSHAKE128
 from Cryptodome.PublicKey import ECC
-# there is probably a way for nacl to read the privatekey.pem file for the secret and not need this.
-from nacl.signing import SigningKey
 
-commandfile = "det.dat"
-vnb = "04/01/2023"
-vna = "04/01/2024"
-DETofC = hex(0x2001003ffe0014050b27c442f9d62167)[2:]
-HIofC = hex(0x4a1232bc278359939e3555bf5393bc5b2abd57c7c3b269622d06c164b9795f07)[2:]
-DETofP = hex(0x2001003ffe0014054a12792a41175eb9)[2:]
-pkeyname = "parent"
-createself = False
+def det_orchid(raa, hda, suiteid, hi):
+	# set some defaults
+	# ORCHID PREFIX = 2001:30/28 = b0010 0000 0000 0001:0000 0000 0011/28
+	# HID = RAA (always 14 bits) + HDA (always 14 bits) = 10 + 20 = b00 0000 0000 1010 + b00 0000 0000 0001 0100
+
+	b_prefix = '0010000000000001000000000011' # RFC 9374 Section 8.2.1
+#	h_prefix = '2001003'
+
+	b_ogaid = f'{suiteid:08b}'
+#	b_ogaid = '00000101' # suiteid of 5, RFC 9374 Section 8.2.2
+	ContextID = unhexlify("00B5A69C795DF5D5F0087F56843F2C40")
+
+
+	#format the HID from RAA and HDA
+#	print("raa:", raa)
+#	print("RAA:", f'{raa:014b}')
+#	print("hda:", hda)
+#	print("HDA:", f'{hda:014b}')
+	b_hid = f'{raa:014b}' + f'{hda:014b}'
+#	print("HID:", b_hid)
+
+	# perform hash with cSHAKE using input data
+	h_orchid_left = unhexlify(b_prefix + b_hid + b_ogaid)
+#	print(h_orchid_left.hex())
+	shake =  cSHAKE128.new(custom = ContextID)
+#	print(type(h_orchid_left),h_orchid_left)
+#	print("hi",type(hi), hi)
+	shake.update((h_orchid_left + hi))
+	h_hash = shake.read(8).hex()
+
+	# format orchid in binary
+	h_orchid = hex(int(b_prefix + b_hid + b_ogaid, 2))[2:] + h_hash
+#	orchid = 2
+
+	print("DET:", h_orchid)
+	# add in ':' for IPv6
+	str_orchid = ':'.join(h_orchid[i:i+4] for i in range(0, len(h_orchid), 4))
+	print("DET:", str_orchid)
+
+	return h_orchid
+
+# If RAA does not use CRL, can use short cert.serial_number
+# If RAA does use CRL, should use large cert.serial_number
+commandfile = "client.dat"
+serialnumberbits = 7
+LOA_str = "1.3.27.16.1.1.0.1"
+vnb = "04/01/2024"
+vna = "04/01/2025"
+clientcsr="hda1"
+cakey="raa"
+certsign = False
+selfsign = False
+entitycert = True
+
+# will derive CA DET from RAA, HDA, etc.
+raa = 16376
+hda = 16376
 
 try:
-	opts, args = getopt.getopt(sys.argv[1:],"hn:p:c:",["commandfile=","pkeyname=","passwd=","vnb=","vna=", "self="])
+	opts, args = getopt.getopt(sys.argv[1:],"hn:p:c:",["serverdat=","commandfile="])
 except getopt.GetoptError:
 	print('Error')
 	sys.ext(2)
@@ -65,23 +129,12 @@ except getopt.GetoptError:
 #	parse the args
 for opt, arg in opts:
 	if opt == '-h':
-		print('endorse.py [-c,--commandfile] <parent commandfile> <parent keyname> [-k --pkeyname, will be appended with prv.pem  -p,--passwd <password> --vnb <Not Before date:04/01/2023> --vna <vna <Not After date:04/01/2023> --self <y/n>]')
+		print('endorse.py --serverdat <Server commanddat> --commandfile <Client commandfile>')
 		sys.exit()
+	elif opt in ("-s", "--serverdat"):
+		serverdat = arg + ".dat"
 	elif opt in ("-c", "--commandfile"):
-		commandfile = arg
-	elif opt in ("-k", "--pkeyname"):
-		pkeyname = arg
-	elif opt in ("-p", "--passwd"):
-		passwd = arg
-	elif opt == '--vnb':
-		vnb = arg
-	elif opt == '--vna':
-		vna = arg
-	elif opt == '--self':
-		if arg == 'y' or arg == 'Y':
-			createself = True
-		else:
-			createself = False
+		commandfile = arg + ".dat"
 
 file1 = open(commandfile, 'r')
 a = True
@@ -92,60 +145,171 @@ while a:
 		a = False
 	exec("%s" % line.strip())
 file1.close()
+clhda = hda
 
-#print(vnb)
-#print(vna)
-#print(hex(DETofC)[2:])
-#print(hex(HIofC)[2:])
-#print(hex(DETofP)[2:])
-#print(pkeyname)
+# read serverfile after commandfile, so client cannot overright server vars
+file2 = open(serverdat, 'r')
+a = True
+while a:
+	line = file2.readline()
+#	print(line.strip())
+	if not line:
+		a = False
+	exec("%s" % line.strip())
+file2.close()
+cahda = hda
 
-element = datetime.datetime.strptime(vnb.strip(),"%m/%d/%Y")
-tuple = element.timetuple()
-vnbtime = time.mktime(tuple)
-#print(vnb, hex(int(vnbtime))[2:])
+clientcsr=clientcsr + "csr.pem"
+cakey=cakey + "prv.pem"
 
-element = datetime.datetime.strptime(vna.strip(),"%m/%d/%Y")
-tuple = element.timetuple()
-vnatime = time.mktime(tuple)
-#print(vna, hex(int(vnatime))[2:])
-#print(hex(DETofC)[2:].zfill(32))
-#print(hex(HIofC)[2:].zfill(64))
-#print(hex(DETofP)[2:].zfill(32))
-
-if createself:
-	pleasesign = hex(int(vnbtime))[2:].zfill(8) + hex(int(vnatime))[2:].zfill(8) + hex(HIofC)[2:].zfill(64) + hex(DETofP)[2:].zfill(32)
-else:
-	pleasesign = hex(int(vnbtime))[2:].zfill(8) + hex(int(vnatime))[2:].zfill(8) + hex(DETofC)[2:].zfill(8) + hex(HIofC)[2:].zfill(64) + hex(DETofP)[2:]
-
-
-# print(pleasesign)
-
-pkfile = pkeyname + "prv.pem"
-
-f = open(pkfile,'rt')
-prkey = ECC.import_key(f.read())
+with open(cakey, "rb") as f:
+	ca_pkkey = f.read()
 f.close()
 
-#	print("seed: ", prkey.seed)
+ca_prkey = load_pem_private_key(ca_pkkey, None)
+ca_prkey_bytes = ca_prkey.private_bytes(
+	encoding=serialization.Encoding.Raw,
+	format=serialization.PrivateFormat.Raw,
+	encryption_algorithm=serialization.NoEncryption()
+	)
+#print("pr", type(ca_prkey_bytes), ca_prkey_bytes)
+ca_pukey = ca_prkey.public_key()
+ca_pukey_bytes = ca_pukey.public_bytes(
+	encoding=serialization.Encoding.Raw,
+	format=serialization.PublicFormat.Raw
+	)
+#print("pu", type(ca_pukey_bytes), ca_pukey_bytes)
+print("CA")
+cadet = det_orchid(raa, cahda, suiteid, ca_pukey_bytes)
+#print("ca orchid", type(cadet),cadet)
 
-sk = SigningKey(prkey.seed)
-mysig = sk.sign(bytes.fromhex(pleasesign)).signature
-# print(len(mysig), str(hexlify(mysig))[2:-1])
+with open(clientcsr, "rb") as f:
+	client_pem_req_data = f.read()
+f.close()
 
-endorsement = pleasesign + str(hexlify(mysig))[2:-1]
-print("Endorsement(", len(endorsement)/2, " bytes):" , endorsement)
+client_csr = x509.load_pem_x509_csr(client_pem_req_data)
+client_csr_pbkey = client_csr.public_key()
+#print("x",client_csr_pbkey)
+client_public_bytes = client_csr.public_key().public_bytes(
+     encoding=serialization.Encoding.Raw,
+     format=serialization.PublicFormat.Raw,)
+#print("client_hi",type(client_public_bytes),client_public_bytes)
+print("Client")
+clientdet = det_orchid(raa, clhda, suiteid, client_public_bytes)
+#print("client orchid", type(clientdet),clientdet)
 
-# CERT RR is **ONLY** for Broadcast, i.e. 136 byte version, not self- 120 bytes...
-# 0c060a2b06010401b43b020606 created by prefixing the following with length 0c:
-# >>> from pyasn1.type import univ
-# >>> from pyasn1.codec.der.decoder import decode as der_decoder
-# >>> from pyasn1.codec.der.encoder import encode as der_encoder
-# >>> import binascii
-# >>> oid = univ.ObjectIdentifier('1.3.6.1.4.1.6715.2.6')
-# >>> s = der_encoder(oid)
-# >>> print(binascii.hexlify(s))
-# 060a2b06010401b43b02060
+cldetb = bytes(clientdet, 'utf-8')
+#print(type(cldetb),cldetb)
+cldeti = int(bytes(clientdet, 'utf-8'),16)
+#print(type(cldeti),cldeti)
+client_hihex = client_public_bytes.hex()
+print("Client HI:", client_hihex)
 
-certrr = (base64.b64encode(bytes.fromhex("0c060a2b06010401b43b020606"+endorsement))).decode('ascii')
-print("CERT RR:\n IN  CERT 254 0 0 (",certrr[:50],"\n",certrr[50:100],"\n",certrr[100:150],"\n",certrr[150:200], ")","\n")
+# Create Endorsement
+
+elementb = datetime.datetime.strptime(vnb.strip(),"%m/%d/%Y")
+tuple = elementb.timetuple()
+vnbtime = time.mktime(tuple)
+#print(type(vnbtime), vnbtime)
+vnbh = hex(int(vnbtime))[2:]
+#print(vnb, len(vnbh), vnbh)
+
+elementa = datetime.datetime.strptime(vna.strip(),"%m/%d/%Y")
+tuple = elementa.timetuple()
+vnatime = time.mktime(tuple)
+#print(vna, hex(int(vnatime))[2:].zfill(8))
+
+pleasesign = hex(int(vnbtime))[2:].zfill(8) + hex(int(vnatime))[2:].zfill(8) + clientdet.zfill(32) + client_hihex.zfill(64) + cadet
+#print(pleasesign)
+pleasesignb = bytes(pleasesign, 'utf-8')
+#print(type(pleasesignb),pleasesignb)
+signature = ca_prkey.sign(pleasesignb)
+#print(type(signature.hex()),signature.hex())
+
+endorsement = pleasesign + signature.hex()
+print("Client Endorsement by CA(", len(endorsement)/2, " bytes):" , endorsement)
+
+#need to convert endorsement to bytes?
+#with open("UA1endor.ment", "wb") as f:
+#	f.write(endorsement)
+
+client_subject_sn = client_csr.subject.get_attributes_for_oid(NameOID.SERIAL_NUMBER)[0].value
+print("client SN:",client_subject_sn)
+
+# Create Lite X.509 cert
+
+builder = x509.CertificateBuilder()
+if entitycert:
+	builder = builder.subject_name(x509.Name([]))
+else:
+	builder = builder.subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "DRIP-" + caname)]))
+	builder = builder.add_extension(
+		x509.BasicConstraints(ca=True, path_length=None), critical=True,
+		)
+builder = builder.not_valid_before(elementb + datetime.timedelta(minutes=1))
+builder = builder.not_valid_after(elementa + datetime.timedelta(hours=23, minutes=59))
+builder = builder.serial_number(random.getrandbits(serialnumberbits))
+builder = builder.public_key(client_csr_pbkey)
+builder = builder.add_extension(
+	x509.SubjectAlternativeName([x509.IPAddress(ipaddress.IPv6Address(cldeti))
+#	,x509.UniformResourceIdentifier('https://cryptography.io')
+	]),critical=True,)
+# need to fix this...
+
+if certsign:
+	builder = builder.add_extension(
+		x509.KeyUsage(digital_signature=False, key_encipherment=False, content_commitment=False,
+					data_encipherment=False, key_agreement=False, key_cert_sign=True, crl_sign=False,
+					encipher_only=False, decipher_only=False),
+		critical=True,
+)
+
+builder = builder.issuer_name(
+    x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, (cadet))]))
+certificate = builder.sign(ca_prkey, None)
+
+with open(clientpem + ".pem", "wb") as f:
+	f.write(certificate.public_bytes(serialization.Encoding.PEM))
+
+# Create PKIX X.509 cert
+
+builder = x509.CertificateBuilder()
+if entitycert:
+	builder = builder.subject_name(x509.Name([]))
+else:
+	builder = builder.subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "DRIP-" + caname)]))
+	builder = builder.add_extension(
+		x509.BasicConstraints(ca=True, path_length=None), critical=True,
+		)
+	policy_info = PolicyInformation(ObjectIdentifier(LOA_str), None)
+	builder = builder.add_extension(
+		CertificatePolicies([policy_info]), critical=False)
+builder = builder.not_valid_before(elementb + datetime.timedelta(minutes=1))
+builder = builder.not_valid_after(elementa + datetime.timedelta(hours=23, minutes=59))
+# If HDA does not use CRL, can use short cert.serial_number
+builder = builder.serial_number(random.getrandbits(serialnumberbits))
+# If HDA does use CRL, should use large cert.serial_number
+#builder = builder.serial_number(x509.random_serial_number())
+builder = builder.public_key(client_csr_pbkey)
+builder = builder.add_extension(
+	x509.SubjectAlternativeName([x509.IPAddress(ipaddress.IPv6Address(cldeti))
+#	,x509.UniformResourceIdentifier('https://cryptography.io')
+	]),critical=True,)
+builder = builder.issuer_name(
+    x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, (cadet))]))
+
+# Create the Subject Key Identifier extension
+#ski = x509.SubjectKeyIdentifier.from_public_key(sub_csr_pbkey)
+if not entitycert:
+	builder = builder.add_extension(
+		x509.SubjectKeyIdentifier(binascii.unhexlify(clientdet)), critical=False)
+if not selfsign:
+	builder = builder.add_extension(
+		x509.AuthorityKeyIdentifier(key_identifier=binascii.unhexlify(cadet), 
+		authority_cert_issuer=None, authority_cert_serial_number=None), critical=False)
+
+certificate = builder.sign(ca_prkey, None)
+
+with open(clientpem + "pkix.pem", "wb") as f:
+	f.write(certificate.public_bytes(serialization.Encoding.PEM))
+
